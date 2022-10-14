d63ba4ea4bdff0d7b750e5b85d5afee4c509ae78
Just Thomas Hiorth Misund <just.thomas.hiorth.misund@coop.no>
2022-10-14 23:17:58 +0200

    Fix typo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect } from "react";
+Piimport React, { useLayoutEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -51,7 +51,7 @@ function App() {
 						<PieChart  {...ageDistributionChart} />
 						<div className="contribute">
 							<h3>Din spennende graf!</h3>
-							<p>Har du lyst til å bidra? Målet er å gjøre dette til et felleskapsprosjekt for kode24 sine lesere. Hvor folke kan lære om visualiseringer og bidra til noe som alle utviklere har nytte av.</p>
+							<p>Har du lyst til å bidra? Målet er å gjøre dette til et felleskapsprosjekt for kode24 sine lesere. Hvor folk kan lære om visualiseringer og bidra til noe som alle utviklere har nytte av.</p>
 							<a className="github-link" href="https://github.com/MaximilianLloyd/kode24-lonn">
 								<img width="50" src="/github.svg" alt="Github logo" />
 							</a>
